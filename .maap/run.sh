ccbdad896d9b8506035ece8abfa5a9cc2c6f0272
rileykk <rileykk@jpl.nasa.gov>
2025-04-08 14:53:11 -0700

    fix env name

--- .maap/run.sh
+++ .maap/run.sh
@@ -5,5 +5,5 @@ basedir=$( cd "$(dirname "$0")" ; pwd -P )
 root_dir=$(dirname "${basedir}")
 
 mkdir -p output
-source activate ingest
+source activate cf2zarr
 python "${root_dir}"/src/cf2zarr.py --sort-dim $1 --pattern "$2" --output $3 --variables ${@:4}
