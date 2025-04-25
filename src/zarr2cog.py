1cbdb07cc6b07d2316257f9ef0d25d3668312743
rileykk <rileykk@jpl.nasa.gov>
2025-04-25 14:25:04 -0700

    zarr2cog fixes

--- src/zarr2cog.py
+++ src/zarr2cog.py
@@ -59,8 +59,8 @@ def main(args):
 
         for time in da[time_c]:
             data = da.sel(time=time)
+            data = data.rio.write_crs("epsg:4326")
             # TODO: For set_spatial_dims should I determine the dim name instead of using the coord name?
-            data = data.rio.write_crs("epsg:4326").set_spatial_dims(x_dim=lon_c, y_dim=lat_c)
             data = data.rio.set_spatial_dims(x_dim=lon_c, y_dim=lat_c)
             dt = time.values.astype('datetime64[s]').item()
             data.attrs = {k.upper(): v for k, v in data.attrs.items()}
