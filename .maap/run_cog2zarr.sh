aa6075e8894a564e26750b6396f8db2064a9c00f
rileykk <rileykk@jpl.nasa.gov>
2025-05-19 16:14:10 -0700

    PGE code and maap entrypoint

---
+++ .maap/run_cog2zarr.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Get current location of build script
+basedir=$( cd "$(dirname "$0")" ; pwd -P )
+root_dir=$(dirname "${basedir}")
+
+CONFIG_FILE=$(ls -d input/*)
+mkdir -p output
+source activate cf2zarr
+python "${root_dir}"/src/zarr2cog.py \
+  --input-s3 $1 \
+  --config $CONFIG_FILE \
+  --pattern $3 \
+  $([[ $4 != "none" ]] && echo --duration $4)\
+  --output $5
--- src/cog2zarr.py
+++ src/cog2zarr.py
@@ -1,21 +1,37 @@
+import argparse
 import os
 import re
+import shutil
+import sys
 from datetime import datetime
+from pathlib import PurePath
 from typing import Tuple
 
+import boto3
 import numpy as np
+import pandas as pd
 import rioxarray
 import xarray as xr
 import yamale
 import yaml
+import zarr
 from odc.geo.geobox import GeoBox
 # from odc.geo.xr import ODCExtensionDs
 from odc.geo.xr import xr_reproject as reproject
 from rioxarray.merge import merge_datasets
 from yamale.validators import Validator, DefaultValidators
 
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+SCHEMA_PATH = os.path.join(SCRIPT_DIR, 'schema', 'geotiff_schema.yaml')
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from src.util import stage_s3
+
 DT_UNITS = ['year', 'month', 'day', 'hour', 'minute', 'second', 'microsecond']
 UNIT_STARTS = dict(year=0, month=1, day=1, hour=0, minute=0, second=0, microsecond=0)
+
+
+staging_dirs = []
 
 
 class PythonRegexValidator(Validator):
@@ -63,6 +79,11 @@ class GeoTiffBandMapValidator(Validator):
     def fail(self, value):
         return (f'{self.get_name()} must be a flat dict mapping incrementing integers starting at one to a set of '
                 f'unique strings')
+
+
+VALIDATORS = DefaultValidators.copy()
+VALIDATORS[PythonRegexValidator.tag] = PythonRegexValidator
+VALIDATORS[GeoTiffBandMapValidator.tag] = GeoTiffBandMapValidator
 
 
 def _open_tiff(path, band_map):
@@ -175,10 +196,172 @@ def test2(schema):
     final_ds.to_netcdf('/Users/rileykk/czdt/czdt-iss-cf2zarr/test.nc')
 
 
-if __name__ == '__main__':
-    package_dir = os.path.dirname(os.path.abspath(__file__))
-    schema_path = os.path.join(package_dir, 'schema', 'geotiff_schema.yaml')
-    
-    print(schema_path)
+def main(args):
+    config_path = args.config
+    pattern = args.pattern
+    output = args.output
 
-    test2(schema_path)
+    session = boto3.Session(profile_name=os.getenv('AWS_PROFILE', None))
+    client = session.client('s3')
+
+    schema = yamale.make_schema(SCHEMA_PATH, validators=VALIDATORS)
+    data = yamale.make_data(config_path)
+
+    yamale.validate(schema, data, strict=True)
+
+    with open(config_path, 'r') as fp:
+        config = yaml.safe_load(fp)
+
+    if config['resolution_deg'] <= 0:
+        raise ValueError('resolution_deg must be greater than zero')
+
+    input_stage_dir = stage_s3(args.input_s3, client)
+    staging_dirs.append(input_stage_dir)
+
+    times = {}
+    filename_pattern = re.compile(config['filename_pattern'])
+
+    input_tiffs = []
+
+    for root, dirs, files in os.walk(input_stage_dir):
+        for filename in files:
+            path = os.path.join(root, filename)
+            if PurePath(path).match(pattern):
+                input_tiffs.append(path)
+
+    if len(input_tiffs) == 0:
+        raise ValueError('no tiffs found in input dir')
+
+    for tiff in input_tiffs:
+        match = filename_pattern.match(os.path.basename(tiff))
+        if match is None:
+            raise ValueError(f'Input tiff {os.path.basename(tiff)} does not match pattern {config["filename_pattern"]}')
+
+        ts_string = match.groupdict()[config['timestamp']['group']]
+        ts = datetime.strptime(ts_string, config['timestamp']['dt_string'])
+
+        if 'round_down_to' in config['timestamp']:
+            ts = ts.replace(
+                **{u: UNIT_STARTS[u] for u in DT_UNITS[DT_UNITS.index(config['timestamp']['round_down_to'])+1:]}
+            )
+
+        print(f'Mapped input {tiff} to time {ts}')
+        times.setdefault(ts, []).append(tiff)
+
+    print(f'Mapped inputs to {len(times)} times')
+
+    gbox = GeoBox.from_bbox(
+        _get_bbox_from_config(config),
+        "epsg:4326",
+        resolution=config['resolution_deg'],
+    )
+
+    reprojected_slices = []
+
+    for timestamp in sorted(times.keys()):
+        print(f'Opening and merging {len(times[timestamp])} tiffs for timestamp {timestamp}')
+        times[timestamp] = merge_datasets(
+            [_open_tiff(f, config['band_map']) for f in times[timestamp]]
+        )
+
+        print('Reprojecting to EPSG:4326')
+        reprojected = reproject(src=times[timestamp], how=gbox, resampling='nearest', dst_nodata=255)
+
+        print('Adding timestamp')
+        reprojected = reprojected.expand_dims('time').assign_coords(
+            time=[np.datetime64(timestamp, 'ns')]
+        )
+
+        print(f'Finished dataset for timestamp:\n{reprojected}')
+        reprojected_slices.append(reprojected)
+
+    final_ds = xr.concat(reprojected_slices, dim='time').sortby('time')
+    print(f'Concatenated all timestamps into single dataset:\n{final_ds}')
+
+    if args.duration is not None:
+        ds_duration = pd.Timedelta((final_ds['time'][-1] - final_ds['time'][0]).data.item())
+
+        print(f'new dataset duration: {ds_duration}')
+
+        if ds_duration > args.duration:
+            print('Dataset duration exceeds max duration provided')
+
+            idx = 0
+
+            while pd.Timedelta((final_ds['time'][-1] - final_ds['time'][idx]).data.item()) > args.duration:
+                idx += 1
+
+            final_ds = final_ds.isel(time=slice(idx, None))
+
+            print(f'Dropped {idx:,} time steps. New dataset duration: '
+                  f'{pd.Timedelta((final_ds["time"][-1] - final_ds["time"][0]).data.item())}')
+
+    chunk_config = (24, 90, 90)
+    print(f'Setting chunk config: {chunk_config}')
+
+    for var in final_ds.data_vars:
+        final_ds[var] = final_ds[var].chunk(chunk_config)
+
+    compressor = zarr.Blosc(cname="blosclz", clevel=9)
+    encoding = {vname: {'compressor': compressor} for vname in final_ds.data_vars}
+
+    print(f'Writing to zarr file: {os.path.join("output", output)}')
+
+    final_ds.to_zarr(
+        os.path.join('output', output),
+        mode='w-',
+        encoding=encoding,
+        consolidated=True,
+        write_empty_chunks=False
+    )
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-i', '--input-s3',
+        required=True,
+        help='S3 URL prefix of input files to stage'
+    )
+
+    parser.add_argument(
+        '-c', '--config',
+        required=True,
+        help='YAML config file for input dataset'
+    )
+
+    parser.add_argument(
+        '-p', '--pattern',
+        default='*.tif',
+        help='Glob pattern to match'
+    )
+
+    parser.add_argument(
+        '-d', '--duration',
+        type=pd.Timedelta,
+        default=None,
+        help='If set, this is the maximum difference in max-min time of the output dataset. Defined as an ISO 8601 '
+             'Duration (or anything else parseable by pandas.Timedelta)'
+    )
+
+    parser.add_argument(
+        '-o', '--output',
+        required=True,
+        help='Output zarr filename'
+    )
+
+    args = parser.parse_args()
+
+    print(args)
+
+    try:
+        main(args)
+    finally:
+        for sd in staging_dirs:
+            try:
+                print(f'Cleaning up staging dir: {sd}')
+                shutil.rmtree(sd)
+            except:
+                print(f'Failed to remove staging dir: {sd}')
+
