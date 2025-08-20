b4b214800d9444c15e3b757f865322031b4f6bd0
rileykk <rileykk@jpl.nasa.gov>
2025-08-20 09:04:17 -0700

    add zarr standard version to zarrcat

--- .maap/run_concat.sh
+++ .maap/run_concat.sh
@@ -6,9 +6,10 @@ root_dir=$(dirname "${basedir}")
 
 mkdir -p output
 source activate cf2zarr
-python "${root_dir}"/src/zarr_concat.py \
+python -u "${root_dir}"/src/zarr_concat.py \
   $([ -n "$1" ] && echo $1)\
   --zarr-manifest $2 \
   --zarr-access $3 \
-  $([[ $4 != "none" ]] && echo --duration $4)\
-  --output $5
+  $([[ $4 != "none" ]] && echo --duration $4) \
+  --zarr-version $5 \
+  --output $6
