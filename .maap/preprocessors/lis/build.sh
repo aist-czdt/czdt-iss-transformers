c94ded54224be623af554a2a0784e5517b5dff77
rileykk <rileykk@jpl.nasa.gov>
2025-09-04 13:42:33 -0700

    update build scripts to fail if conda install fails

--- .maap/preprocessors/lis/build.sh
+++ .maap/preprocessors/lis/build.sh
@@ -1,10 +1,15 @@
 #/usr/bin/env bash
 
+set -x
+
 # Get current location of build script
 basedir=$( cd "$(dirname "$0")"/../.. ; pwd -P )
 root_dir=$(dirname "${basedir}")
 
 pushd "${root_dir}"/preprocessors/lis
+
+set -e
+
 conda env update -f environment.yaml
 conda list -n lis_preprocessor
 popd
