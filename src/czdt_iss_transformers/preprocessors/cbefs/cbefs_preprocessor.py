5fb5b8dfb1f4a9929a5179c161011d82c0543d49
rileykk <rileykk@jpl.nasa.gov>
2025-10-13 08:46:53 -0700

    remove reserved attribute

--- src/czdt_iss_transformers/preprocessors/cbefs/cbefs_preprocessor.py
+++ src/czdt_iss_transformers/preprocessors/cbefs/cbefs_preprocessor.py
@@ -120,10 +120,9 @@ def main(args):
 
         print(ds_regridded.attrs)
 
-        if 'NAME' in ds_regridded.attrs:
-            # NAME is a reserved attribute in NC4 standards so must be replaced
-            ds_regridded.attrs['name'] = ds_regridded.attrs['NAME']
-            del ds_regridded.attrs['NAME']
+        if '_NCProperties' in ds_regridded.attrs:
+            # _NCProperties is a reserved attribute in NC4 standards so must be replaced or removed
+            del ds_regridded.attrs['_NCProperties']
 
         print("\n--- Regridded Dataset on WGS84 Grid ---")
         print(ds_regridded)
